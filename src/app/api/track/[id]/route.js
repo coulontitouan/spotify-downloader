106a3cccbca219c1b34a3de1b15364ac74531e31
Livreur <coulontitouan@gmail.com>
2025-01-30 23:17:25 +0100

    modif

--- src/app/api/track/[id]/route.js
+++ src/app/api/track/[id]/route.js
@@ -2,22 +2,22 @@ import { getTrack } from "@/lib/spotify";
 import { NextResponse } from "next/server";
 
 export const GET = async (request, { params }) => {
-  const id = params.id;
+    const id = params.id;
 
-  if (!id)
-    return NextResponse.json(
-      { message: "Provide a track ID" },
-      { status: 400 }
-    );
+    if (!id)
+        return NextResponse.json(
+            { message: "Provide a track ID" },
+            { status: 400 }
+        );
 
-  try {
-    const track = await getTrack(id);
-    return NextResponse.json(track);
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json(
-      { message: "There has been an error fetching the track" },
-      { status: 500 }
-    );
-  }
+    try {
+        const track = await getTrack(id);
+        return NextResponse.json(track);
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json(
+            { message: "There has been an error fetching the track" },
+            { status: 500 }
+        );
+    }
 };
--- src/lib/spotify.js
+++ src/lib/spotify.js
@@ -1,57 +1,63 @@
 import axios from "axios";
 
 export const getToken = async () => {
-  const response = await axios.post(
-    "https://accounts.spotify.com/api/token",
-    "grant_type=client_credentials",
-    {
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      auth: {
-        username: process.env.CLIENT_ID,
-        password: process.env.CLIENT_SECRET,
-      },
-    }
-  );
+    const response = await axios.post(
+        "https://accounts.spotify.com/api/token",
+        "grant_type=client_credentials",
+        {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            auth: {
+                username: process.env.CLIENT_ID,
+                password: process.env.CLIENT_SECRET,
+            },
+        }
+    );
 
-  return response.data.access_token;
+    return response.data.access_token;
 };
 
 export const getRequest = async (url) => {
-  const token = await getToken();
-  const response = await axios.get(url, {
-    headers: { Authorization: `Bearer ${token}` },
-  });
+    const token = await getToken();
+    try {
+        const response = await axios.get(url, {
+            headers: { Authorization: `Bearer ${token}` },
+        });
 
-  return response.data;
+        return response.data;
+    }
+    catch (error) {
+        console.error(`Error fetching: ${url}`);
+    }
+    return response.data;
 };
 
 export async function getPlaylist(id) {
-  try {
-    let playlist = await getRequest(
-      `https://api.spotify.com/v1/playlists/${id}`
-    );
+    try {
+        let playlist = await getRequest(
+            `https://api.spotify.com/v1/playlists/${id}`
+        );
 
-    let { next } = playlist.tracks;
-    while (next) {
-      const nextTracks = await getRequest(next);
-      playlist.tracks.items.push(...nextTracks.items);
+        let { next } = playlist.tracks;
+        while (next) {
+            const nextTracks = await getRequest(next);
+            playlist.tracks.items.push(...nextTracks.items);
 
-      next = nextTracks.next;
+            next = nextTracks.next;
+        }
+
+        return playlist;
+    } catch (error) {
+        console.error(`Error fetching playlist: ${id}`);
     }
-
-    return playlist;
-  } catch (error) {
-    console.error(`Error fetching playlist: ${id}`);
-  }
 }
 
 export async function getTrack(id) {
-  try {
-    const track = await getRequest(`https://api.spotify.com/v1/tracks/${id}`);
-    return track;
-  } catch (error) {
-    console.error("Error fetching track:", error);
-  }
+    try {
+        const track = await getRequest(`https://api.spotify.com/v1/tracks/${id}`);
+        return track;
+    } catch (error) {
+        console.error("Error fetching track:", error);
+    }
 }
